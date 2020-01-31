013f962a4d6d8a166af0627f42749cff89dece04
kajal-patel97 <57909923+kajal-patel97@users.noreply.github.com>
2020-01-31 10:20:44 +0000

    posts working

--- modules/app_tier/main.tf
+++ modules/app_tier/main.tf
@@ -46,6 +46,12 @@ resource "aws_security_group" "app_security_kp" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  egress {
+    from_port = 0
+    to_port =  0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
 
 }
--- modules/db_tier/main.tf
+++ modules/db_tier/main.tf
@@ -8,29 +8,17 @@ resource "aws_security_group" "db_security_kp" {
     from_port = 27017
     to_port =  27017
     protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    security_groups = ["${var.app_sg_id}"]
 
   }
   ingress {
     from_port = 22
     to_port =  22
     protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    security_groups = ["${var.app_sg_id}"]
   }
 
-  ingress {
-    from_port = 80
-    to_port =  80
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 
-  ingress {
-    from_port = 3000
-    to_port =  3000
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 
   tags = {
     Name = var.db_name
@@ -48,22 +36,7 @@ resource "aws_subnet" "db_subnet" {
   }
 }
 
-#Route table
-resource "aws_route_table" "db_route" {
-vpc_id = var.vpc_id
-route {
-  cidr_block = "0.0.0.0/0"
-  gateway_id = var.ig_id
-  }
-  tags = {
-  Name = var.db_name
-  }
-}
-# Route table associations
-resource "aws_route_table_association" "db_assoc" {
-subnet_id = aws_subnet.db_subnet.id
-route_table_id = aws_route_table.db_route.id
-}
+
 
 
 # Launch an Instance
@@ -76,9 +49,5 @@ resource "aws_instance" "db_instance" {
   associate_public_ip_address = true
   tags = {
     Name = var.db_name
-  }
-
-provisioner "local-exec" {
-  command = "echo ${aws_instance.db_instance.public_ip} > ip_address.txt"
   }
 }
